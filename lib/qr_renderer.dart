4222f5538e4548ced4a6abe7730455e4339923b2
Maks Ismoilov <maks.ismoilov0618@gmail.com>
2019-09-15 01:09:41 +0300

    Edited qr_renerer for generating qr code. added permissions to manifest. added dependency for rendering qr code.

--- lib/qr_renderer.dart
+++ lib/qr_renderer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class QrRenderer extends StatelessWidget {
+
+class QrRenderer extends StatefulWidget {
   final String invoiceId;
   final String address;
   final int amount;
@@ -12,7 +14,24 @@ class QrRenderer extends StatelessWidget {
   });
 
   @override
+  State<StatefulWidget> createState() => GenerateQrCode();
+}
+
+class GenerateQrCode extends State<QrRenderer> {
+
+  GlobalKey globalKey = new GlobalKey();
+  String _data = "Hello NetworkOnMainThreadException";
+
+  @override
   Widget build(BuildContext context) {
-    return Text('[QR]');
+    final bodyHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom;
+    return RepaintBoundary(
+      key: globalKey,
+      child: QrImage(
+        data: _data,
+        size: 0.5 * bodyHeight,
+      ),
+    );
   }
+
 }
