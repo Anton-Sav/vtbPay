c05f6edd2629bd6d4ad3bade2d73f7fee849c34d
eisayfutdinova <eisayfutdinova@edu.hse.ru>
2019-09-15 11:17:47 +0300

    make invoice page design

--- lib/invoice_page.dart
+++ lib/invoice_page.dart
@@ -33,7 +33,6 @@ class _InvoicePageState extends State<InvoicePage> {
             '¥EEZY PAY',
             style: TextStyle(
               fontSize: 20,
-              height: 1.5,
               fontFamily: 'Montserrat',
               color: Color.fromRGBO(255, 71, 58, 50),
             ),
@@ -41,43 +40,107 @@ class _InvoicePageState extends State<InvoicePage> {
           centerTitle: true,
           backgroundColor: Colors.white,
         ),
-        body: SizedBox(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Введите общую сумму счёта',
-                  ),
-                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Введите сумму';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    paymentSum = int.parse(value);
-                  },
-                ),
-                RaisedButton(
-                  child: Text('Собрать деньги'),
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      _formKey.currentState.save();
-                      Nav(context).push(
-                        MakeInvoicePage(
-                          paymentSum: paymentSum,
+        body: SingleChildScrollView(
+          child:  Container(
+            child: SizedBox(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: Image(
+                        image: AssetImage('assets/invoice.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical:6.0, horizontal: 16.0),
+                      child: Text(
+                        'Поделить\nсчет',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontFamily: 'Montserrat',
+                          color: Colors.black,
                         ),
-                      );
-                    }
-                  },
-                )
-              ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                      child: Text(
+                        'Invoice',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                          color: Color.fromRGBO(	212, 212, 212, 50),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'RUB',
+                        ),
+                        inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Введите сумму';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          paymentSum = int.parse(value);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 16.0),
+                      child: Text(
+                        'Введите общую сумму\n счета',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'Montserrat',
+                          color: Color.fromRGBO(	212, 212, 212, 50),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Center(
+                        child: SizedBox(
+                          width: 260,
+                          height: 42,
+                          child:RaisedButton(
+                            color: MyColors.red,
+                            child: Text(
+                              'Собрать деньги',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Montserrat',
+                                fontSize: 12,
+                              ),
+                            ),
+                            onPressed: () {
+                              if (_formKey.currentState.validate()) {
+                                _formKey.currentState.save();
+                                Nav(context).push(
+                                  MakeInvoicePage(
+                                    paymentSum: paymentSum,
+                                  ),
+                                );
+                              }
+                            },
+                          ) ,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ));
+        )
+    );
   }
 }
